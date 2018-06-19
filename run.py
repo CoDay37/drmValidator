3269290bdab56d669155c155f990fbef392f035a
Your Name <cday@ncta.com>
2018-06-19 14:14:59 -0400

    1)Added Recursion 2)Optimized Code

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from tools.pageParser import getAllLinks, findLinks, controller
 
 
 #input = input("Enter a URL to be checked for video sources.")
-input = 'http://www.theverge.com'
+url = 'https://www.espn.com/watch/?id=3345739'
+url2 = 'https://www.nbcsports.com'
+url3 = 'https://www.reddit.com'
 
-(controller(input))
+(controller(url3))
