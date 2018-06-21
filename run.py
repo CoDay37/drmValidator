b958c45c000a6566140064452f69c4b6d52a206d
Your Name <cday@ncta.com>
2018-06-21 15:10:07 -0400

    Optimzation

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ from tools.pageParser import getAllLinks, findLinks, controller
 #input = input("Enter a URL to be checked for video sources.")
 url = 'https://www.espn.com/watch/?id=3345739'
 url2 = 'https://www.nbcsports.com'
-url3 = 'https://www.reddit.com'
+url3 = "https://www.theverge.com/"
+url4 = 'http://watch.hgtv.com/live/?ic1=TVE_HGTV_Shows-ChannelPage-RightRail'
+url5 = 'https://www.cbs.com'
+url6 = 'http://www.mtv.com/episodes/y7pn7w/jersey-shore-family-vacation-meatball-training-day-season-1-ep-110'
 
-(controller(url3))
+(controller(url5))
--- tools/pageParser.py
+++ tools/pageParser.py
@@ -16,69 +16,100 @@ parseHTML = set()
 videoLinks = set()
 randomLinks = set()
 holdLinks = set()
-lengthOf = len(parseHTML)
+htmlLinks = 0
+totalL = 0
+base_URL = 'https://www.nbc.com'
+payload = {#For Xfinity
+    'user': 'nctatech',
+    'passwd': 'TechLab!'
+}
+payload2 = {
+    'IDToken1':'Men254@charter.net',
+    'IDToken2':'Adam1041'
+}
 
 
 def getAllLinks(parseHTML):
         apple = parseHTML.copy()
-        print("Length of parseHTML: ", len(parseHTML))
+        chc = (len(apple))
         for element in apple:
-                hold = element[2:-2]
-                print("*******", element)
-                findLinks(hold)
+                chc-=1
+                print("Remaining: ", chc)
+                print(element)
+                cleanLink = (element[2:-2])
+                findLinks(cleanLink)
+                parseHTML.remove(element)
+        linkChecker()
+
+        #for element in apple:
+        #        hold = element[2:-2]
+         #       findLinks(hold)
 
 def findLinks(link):
-        aLink = 'http'
-        bLink = 'https'
-        r = requests.get(link, headers={'User-Agent': USER_AGENT})
-        print("Status: ", r.status_code)
-        data = r.text
-        soup = BeautifulSoup(data,'html.parser')
-        for link in soup.findAll("a"):
-                sLink = link.get('href') 
-                unCheckedLinks.add(sLink)
-        for link in soup.findAll("link"):
-                sLink2 = link.get('href')
-                unCheckedLinks.add(sLink2)
-        for element in unCheckedLinks:
-                holding = str(element)
-                url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', holding)
-                link = str(url)
-                if not "http" in link or not "https" in link:
-                        continue
-                else:
-                        holdLinks.add(link)
-        unCheckedLinks.clear()
-        linkChecker()
-        #TO_DO TOMORROW
-        #duplicate set and use to control recurisive loop
-        #USE request.sessions to login with username/pW
+        with requests.Session() as s:
+                p = s.post(link, data=payload)
+                #r = requests.get(link, headers={'User-Agent': USER_AGENT})
+                data = p.content
+                soup = BeautifulSoup(data,'html.parser')
+                soup.prettify
+                global totalL
+                totalL+=1
+                #print("Total Number of Links Parsed: ", totalL)
+
+                print("Status:", p.status_code)
+                links = soup.find_all('a',href=True)
+                links2 = soup.find_all('link',href=True)
+                for i in links2:
+                        link = i.get('href')
+                        unCheckedLinks.add(link)
+                        #print("i", link)
+                for link in soup.findAll("a"):
+                        sLink = link.get('href')
+                        #print("slink",sLink)
+                        unCheckedLinks.add(sLink)
+                for link in soup.findAll("link"):
+                        sLink2 = link.get('href')
+                        #print('slink2',sLink2)
+                        unCheckedLinks.add(sLink2)
+                for element in unCheckedLinks:
+                        holding = str(element)
+                        url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', holding)
+                        link = str(url)
+                        if not "http" in link or not "https" in link:
+                                continue
+                        else:
+                                holdLinks.add(link)
+                unCheckedLinks.clear()
+                #TO_DO TOMORROW
+                #ENSURE THAT PARSER STAYS ON WEBSITE
+
 
 def linkChecker():
-        print("Length of HoldLinks: ", len(holdLinks))
-        for element in (holdLinks):
+        for element in holdLinks:
                 if 'ico' in element or 'png' in element or 'jpg' in element or "woff2" in element or 'css' in element or 'js' in element or "xml" in element:
                         randomLinks.add(element)
-                elif 'mp4' in element or 'mpeg' in element or 'wmv' in element or 'mov' in element:
+                elif 'mp4' in element or 'mpeg' in element or 'wmv' in element or '.mov' in element:
                         videoLinks.add(element)
-                if not element:
+                        print(element)
+                elif 'twitter' in element or 'facebook' in element or 'google.com' in element or 'instagram.com' in element:
                         continue
-                else: 
+                elif 'cbs' not in element:
+                        continue
+                else:
                         parseHTML.add(element)
+                        print(element)
+        print("Length of parseHTML", len(parseHTML))
         holdLinks.clear()
-                
-                        
+
 def controller(url):
         findLinks(url)
-        #linkChecker()
-        getAllLinks(parseHTML)
+        linkChecker()
+        print("### GETTING ALL LINKS")
+        control = range(len(parseHTML))
+        controlll = 2
+        for i in control:
+                getAllLinks(parseHTML)                
         print("# of unchecked: ", len(unCheckedLinks))
         print("# of vid", len(videoLinks))
         print("# of random links", len(randomLinks))
-
-        for elemet in randomLinks:
-                print("**", elemet)
-
-        for element in videoLinks:
-                print(element)
 
